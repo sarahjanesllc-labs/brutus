f757eac683406396ecdd9aa127949a46e5ce2683
Adam Stokes <adam.stokes@ubuntu.com>
2014-05-16 22:23:14 -0400

    stub routes
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- brutus/app.py
+++ brutus/app.py
@@ -63,16 +63,16 @@ class Application(tornado.web.Application):
         urls = [
             (r"/", "brutus.views.index"),
             (r"/about", "brutus.views.about"),
-            (r"/get-started", "brutus.views.get_started"),
+            (r"/start", "brutus.views.start"),
             (r"/pricing", "brutus.views.pricing"),
             (r"/login", "brutus.views.login"),
             (r"/logout", "brutus.views.logout"),
             # Consumer routes
-            (r"/explore", "brutus.consumer.views.index"),
+            # (r"/explore", "brutus.consumer.views.index"),
             # Vendor routes
             (r"/vendor", "brutus.vendor.views.index"),
             # Management routes
-            (r"/manage", "brutus.manager.views.index"),
+            # (r"/manage", "brutus.manager.views.index"),
         ]
         settings = dict(
             template_path=os.path.join(self.cfg.app_path, "templates"),
--- brutus/views.py
+++ brutus/views.py
@@ -2,10 +2,28 @@
 
 from brutus.app import Handler
 
+
 class index(Handler):
     def get(self):
-        self.render_json({'status':'it works!'})
+        self.render_json({'status': 'it works!'})
+
 
 class about(Handler):
     def get(self):
-        self.render_json({'status':'about us!'})
+        self.render_json({'status': 'about us!'})
+
+
+class start(Handler):
+    pass
+
+
+class pricing(Handler):
+    pass
+
+
+class login(Handler):
+    pass
+
+
+class logout(Handler):
+    pass
